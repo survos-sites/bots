edc3366eb9aa356e9863edc2b0452614d3aa6ec0
Tac Tacelosky <tacman@gmail.com>
2025-06-17 13:45:51 -0400

    add inspector

--- src/Service/AgentRegistry.php
+++ src/Service/AgentRegistry.php
@@ -7,6 +7,8 @@ use App\Agent\AppAgentInterface;
 use App\Agent\AppRAGAgent;
 use App\Agent\MeiliRAG;
 use App\Agent\RappAgent;
+use Inspector\Inspector;
+use NeuronAI\Observability\AgentMonitoring;
 use NeuronAI\RAG\RAG;
 use Psr\Container\ContainerInterface;
 use Symfony\Component\DependencyInjection\Attribute\AutowireLocator;
@@ -16,6 +18,7 @@ use Symfony\Contracts\Service\ServiceCollectionInterface;
 class AgentRegistry
 {
     public function __construct(
+        private Inspector $inspector,
 //        private RappAgent $rappAgent,
 //        #[AutowireLocator('app.agent', indexAttribute: 'index')]
 //        private ContainerInterface $agents,
@@ -39,6 +42,8 @@ class AgentRegistry
 
     public function get(string $code): AgentInterface|AppAgentInterface|AppRAGAgent
     {
-        return $this->agents->get($code);
+        $agent = $this->agents->get($code);
+        $agent->observe(new AgentMonitoring($this->inspector));
+        return $agent;
     }
 }
